be37c7508f1390988e69b88640b6bda8da6ae6d7
Chris <chauyuhin1998@gmail.com>
2023-07-27 17:30:21 +0200

    stable version? idk

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -11,7 +11,7 @@ const path = require('path');
 
 
 // The BackEnd port may be set on the command line
-const port = process.argv.length > 2 ? process.argv[2] : 8080;
+const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
 // JSON body parsing using built-in middleware
 app.use(express.json());
