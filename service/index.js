63a2a256c37f7660fe5a961eb1d009a2714b5737
Chris <chauyuhin1998@gmail.com>
2023-07-28 17:22:58 +0200

    stable version? idk

--- service/index.js
+++ service/index.js
@@ -191,10 +191,10 @@ app.use(function (err, req, res, next) {
     res.status(500).send({type: err.name, message: err.message});
 });
 
-// Return the application's default page if the path is unknown
-app.use((_req, res) => {
-    res.sendFile('index.html', {root: __dirname + "/build"});
-});
+// // Return the application's default page if the path is unknown
+// app.use((_req, res) => {
+//     res.sendFile('index.html', {root: __dirname + "/build"});
+// });
 
 
 // setAuthCookie in the HTTP response
