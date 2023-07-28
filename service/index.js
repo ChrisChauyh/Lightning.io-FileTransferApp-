d3bec926bcc8c83681dc481daf53e37fa7d04e85
Chris <chauyuhin1998@gmail.com>
2023-07-28 16:56:33 +0200

    stable version? idk

--- service/index.js
+++ service/index.js
@@ -214,7 +214,6 @@ const httpService = app.listen(port, () => {
 app.use(express.static(path.join(__dirname, '../build')));
 
 app.get("*", function (req, res) {
-
     res.sendFile(
         path.join(__dirname, "../build/index.html")),
         function (err) {
